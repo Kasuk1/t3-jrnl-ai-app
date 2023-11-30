7e1d60258d9a89891e62ce15e0de6ce3efa6ebe6
Igor Chinchay Farronay <igor2008_11@hotmail.com>
2023-11-30 16:14:40 -0500

    First fixes with vercel

--- src/components/NoEntries.tsx
+++ src/components/NoEntries.tsx
@@ -2,9 +2,9 @@ import { PencilIcon } from "@heroicons/react/24/solid";
 
 export const NoEntries = () => {
   return (
-    <div className="font-monserrat mx-auto flex w-3/4 flex-row items-center justify-center gap-8 rounded-sm border-gray-400 bg-slate-800/30 p-10 text-lg text-gray-50 md:w-1/2">
+    <div className="mx-auto flex w-3/4 flex-row items-center justify-center gap-8 rounded-sm border-gray-400 bg-slate-800/30 p-10 font-monserrat text-lg text-gray-50 md:w-1/2">
       <PencilIcon width={40} />
-      <p>You don't have any entries yet...</p>
+      <p>You don&apos;t have any entries yet...</p>
     </div>
   );
 };
